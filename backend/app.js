12fdb9dbfe2cb3bf837bbf169c56f5376c133570
Shayak Karan <shayak2k@gmail.com>
2023-04-30 15:12:30 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const cors =require('cors');
+const { db } = require('./db/db');
+const {readdirSync} = require('fs');
+const path = require('path');
+const app = express()
+
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5002
+
+
+app.use(express.json())
+app.use(cors())
+
+
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+app.use(express.static(path.join(__dirname, './frontend/build')))
+
+app.get("*", function(req,res){
+    res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+});
+
+const server = () => {
+    db()
+    app.listen(PORT, () => {
+console.log('listening to port:', PORT);
+})
+}
+
+server()
